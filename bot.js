0477fca34a52d4856b8a95e9320cab6cf0c1af80
Stephan09 <38815320+Stephan09@users.noreply.github.com>
2018-10-14 14:15:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,42 +5,12 @@ const Discord = require('discord.js');
 
 
 
-
-
-
-
-
-
-
-
-
 const client = new Discord.Client();
 
 
 
 
-
-
-
-
-
-
-
-const prefix = 't!';
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
+const prefix = '?';
 
 
 
@@ -51,386 +21,84 @@ const prefix = 't!';
 
 
 client.on('ready', () => {
-
-
-
     client.user.setStatus('available')
-
-
-
     client.user.setPresence({
-
-
-
         game: {
-
-
-
             name: 'Creativerse',
-
-
-
             type: "PLAYING",
-
-
-
         }
-
-
-
     });
-
-
-
 });    
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
 client.on('message', message => {
-
-
-
     if (message.content === 'ping') {
-
-
-
        message.reply('pong');
-
-
-
        }
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 
 client.on('ready', () => {
-
-
-
 	console.log("Bot ist fertig xD\n\n")
-
-
-
 	});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 client.on('message', message => {
-
-
-
-    if (message.content === prefix + 'twitch') {
-
-
-
-       message.channel.send('Here ist the link to Playfulcorp twitch channel: https://www.twitch.tv/playfulcorp ;)');
-
-
-
-       }
-
-
-
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-
-
-
-	
-
-
-
 	if(message.content.toLowerCase() === '<@497685236763590656>') { if(message.author.bot) { return; }
-
-
-
-	 
-
-
-
 	         var embed = new Discord.RichEmbed()
-
-
-
-			 
-
-
-
-			 .addField('test:smile:  ', 'only test')
-
+			 .addField('test:smile:  ', '?hilfe')
 			 .setColor('GREEN')
-
-
-
-			 
-
-
-
-			message.channel.sendEmbed(embed);
-
-
-
-	
-
-
-
+			message.channel.sendEmbed(embed);						      
 	}
-
-
-
-	
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 
 client.on('guildMemberAdd', member => {
-
-
-
-		const channel = member.guild.channels.find(ch => ch.name === 'chat');
-
-
-
+		const channel = member.guild.channels.find(ch => ch.name === 'willkommen');
         if (!channel) return;
+		channel.send(`Herzlich Willkommen beim Server Deutschsprachige Baumaschinen, ${member}
+Discord Server Regeln:
+
+- Nicht beleidigen
+- Nicht mobben
+- kein Rassismus
+- Freundlicher Umgang im Chat
+- Wenn ein Admin gebraucht wird bitte in den #👀admin-gesucht channel schreiben, @Admin  eintippen wenn es dringend ist.
+- Server Kommandos: !hilfe, dann erscheint eine Liste mit Kommandos.
 
 
-
-		channel.send(`${member}Willkommen!`);
-
-
-
+Wir wünschen viel Spaß beim bauen euer Server Team von den Deutschsprachigen Baumaschinen Server aus Creativerse !`);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
 
 
 
 client.on('message', message => {
-
-
-
-	
-
-
-
-	if(message.content.toLowerCase() === prefix + 'help') { if(message.author.bot) { return; }
-
-
-
-	 
-
-
-
+	if(message.content.toLowerCase() === prefix + 'hilfe') { if(message.author.bot) { return; }
 	         var embed = new Discord.RichEmbed()
-
-
-
-			 
-
-
-
-			 .addField('du brauchst hilfe? ','mach t!hilfe für hilfe')
-
-
-
+			 .addField('du brauchst hilfe? ','meine commandos')
 			 .setColor('GREEN')
-
-
-
-			 
-
-
-
 			message.channel.sendEmbed(embed);
-
-
-
-	
-
-
-
 	}
-
-
-
-	
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 
 // THIS  MUST  BE  THIS  WAY
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
